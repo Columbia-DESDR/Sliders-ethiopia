3235f5fc3f36e0a33e348df8efc8184180df356f
Ramya Subramanian <Ramya@ramyas-macbook.lan>
2023-05-24 10:32:04 -0700

    1 parent 2893191 commit 18723e2

---
+++ docs/internal/immutable/chunks/paths.48744069.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1wvsbix)==null?void 0:s.base)??"/desdr-ethiopia-demo";var e;const t=((e=globalThis.__sveltekit_1wvsbix)==null?void 0:e.assets)??a;export{t as a,a as b};
