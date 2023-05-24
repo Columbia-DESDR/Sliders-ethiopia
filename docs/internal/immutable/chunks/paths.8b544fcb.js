884edc64929d1676b55b46d60a7cb0077634c411
Ramya Subramanian <Ramya@ramyas-macbook.lan>
2023-05-24 10:39:52 -0700

    commit 2cd8e7c1a10b6e451c6984f00abf8a86a8b65fe

---
+++ docs/internal/immutable/chunks/paths.8b544fcb.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_uaf35c)==null?void 0:s.base)??"/desdr-ethiopia-demo";var a;const t=((a=globalThis.__sveltekit_uaf35c)==null?void 0:a.assets)??e;export{t as a,e as b};
